9c1edf20216d7ddb17dfc9293e4944542534d877
Chiemelie10 <chiemelieeze@yahoo.com>
2023-07-27 19:33:24 +0000

    Updated README.md file

---
+++ 0x03-log_parsing/stats.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""This script reads stdin line by line and computes metrics."""
+
+import sys
+
+
+if __name__ == "__main__":
+    total_size = 0
+    status_count = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+
+    def print_statistics(total_size, status_count):
+        """Prints input to stdout."""
+
+        print("File size: {}".format(total_size))
+
+        sorted_status_codes = sorted(status_count.items())
+
+        for code, count in sorted_status_codes:
+            print("{}: {}".format(code, count))
+
+    try:
+        line_counter = 0
+
+        for line in sys.stdin:
+            line = line.strip()
+            parts = line.split(" ")
+            line_counter += 1
+
+            if len(parts) == 9:
+                try:
+                    status_code = int(parts[-2])
+
+                    if status_code not in status_count:
+                        continue
+                except ValueError:
+                    line_counter -= 1
+                    continue
+
+                try:
+                    file_size = int(parts[-1])
+                except ValueError:
+                    line_counter -= 1
+                    continue
+
+                total_size += file_size
+
+                if status_code in status_count:
+                    status_count[status_code] += 1
+                else:
+                    status_count[status_code] = 1
+            else:
+                line_counter -= 1
+
+            if line_counter % 10 == 0:
+                print_statistics(total_size, status_count)
+    except BaseException:
+        pass
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_count)
+        raise
+    print_statistics(total_size, status_count)
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Solutions to some technical interview questions
 1) Pascal's Triangle
 2) Lock Boxes
 3) Log Parsing
+4) UTF-8 Validation
