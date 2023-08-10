573d4371e1a67d0e861594a1f8c3e545f91649c3
Chiemelie10 <chiemelieeze@yahoo.com>
2023-08-10 19:14:59 +0000

    Task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,26 +7,23 @@
  */
 
 const request = require('request');
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-// Makes API request, sets async to allow await promise
-request(filmURL + filmNum, async (err, res, body) => {
-  if (err) return console.error(err);
-
-  // find URLs of each character in the film as a list obj
-  const charURLList = JSON.parse(body).characters;
-
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
-  for (const charURL of charURLList) {
-    await new Promise((resolve, reject) => {
-      request(charURL, (err, res, body) => {
-        if (err) return console.error(err);
-
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
       });
-    });
+      console.log(res);
+    }
   }
 });
